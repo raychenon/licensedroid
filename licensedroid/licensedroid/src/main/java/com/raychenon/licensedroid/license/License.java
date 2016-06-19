d9c2a5edb36fa246363d3dd09fc02bfc534a1309
Raymond Chenon <raymond.chenon@gmail.com>
2016-06-19 22:36:41 +0200

    add enums in License

--- licensedroid/licensedroid/src/main/java/com/raychenon/licensedroid/license/License.java
+++ licensedroid/licensedroid/src/main/java/com/raychenon/licensedroid/license/License.java
@@ -8,6 +8,8 @@ public enum License {
     APACHE_2(new ApacheLicense20()),
     BSD2(new BSD2ClauseLicense()),
     BSD3(new BSD3ClauseLicense()),
+    LGPL21(new LGPL21License()),
+    LGPL30(new LGPL30License()),
     MIT(new MITLicense()),
     MOZILLA_PUBLIC_LICENSE(new MozillaPublicLicense20());
 
