d8cc9292467e9fc1388dd897b4a6d65ed9a1dacd
Raymond Chenon <raymond.chenon@gmail.com>
2016-07-07 01:23:26 +0200

    set a title by extending AppCompatActivity

--- licensedroid/app/src/main/java/com/raychenon/licensedroid/sample/StartActivity.java
+++ licensedroid/app/src/main/java/com/raychenon/licensedroid/sample/StartActivity.java
@@ -2,7 +2,7 @@ package com.raychenon.licensedroid.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 /**
  * @author Raymond Chenon
  */
-public class StartActivity extends FragmentActivity {
+public class StartActivity extends AppCompatActivity {
 
     Button activityBtn;
     Button dialogBtn;
